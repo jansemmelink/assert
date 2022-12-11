4e01476cfa798d9050e66fca0acb2f4bd635f3a8
Jan Semmelink <jan.semmelink@gmail.com>
2022-12-11 09:31:12 +0200

    Update

--- string.go
+++ string.go
@@ -2,24 +2,21 @@ package assert
 
 import (
 	"testing"
-
-	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
-func String(t *testing.T, title string, exp,got string) {
+func String(t *testing.T, title string, exp, got string) {
 	if exp != got {
 		t.Errorf("Expected: (len=%d) %s", len(exp), exp)
 		t.Errorf("Got:      (len=%d) %s", len(got), got)
 		if len(exp) != len(got) {
 			t.Fatalf("%s got len %d != expected %d", title, len(got), len(exp))
 		}
-		for i := 0; i < len(exp) {
+		for i := 0; i < len(exp); i++ {
 			if exp[i] != got[i] {
 				t.Errorf("Got:      (len=%d) %s", len(got), got)
 				t.Fatalf("%s different at [%d]: got=\"%s\" != expected=\"%s\"", title, i, got, exp)
 			}
 		}
-		t.Fatalf("%s not the same", title)//not expected to get here...
+		t.Fatalf("%s not the same", title) //not expected to get here...
 	}
-	return nil
 }
